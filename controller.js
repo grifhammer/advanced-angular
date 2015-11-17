37baec93527ed3fb90a5cb567fef56870b6f2509
Griffin Hammer <grifhammer@gmail.com>
2015-11-17 11:32:15 -0500

    Added Routing
    Added routing to file and removed HTML

--- controller.js
+++ controller.js
@@ -1,8 +1,27 @@
-var countryApp = angular.module('countryApp', []);
+var countryApp = angular.module('countryApp', ['ngRoute']);
+
+countryApp.config(function($routeProvider){
+	$routeProvider.when('/', {
+		template: '<ul><li ng-repeat="country in countries">{{country.name}}</li></ul>',
+		controller: 'countryController'
+	}).
+	when('/:countryName',{
+		template: '<h1>Country detail will go here.</h1>',
+		controller: 'countryDetailController'
+	}).
+	otherwise({
+		redirectTo: '/'
+	})
+});
 
 countryApp.controller('countryController', function($scope, $http){
 	$http.get('countries.json').success(function(countryData){
 		$scope.countries = countryData;
-		
+		console.log($scope.countries)
+
 	});
-})
+});
+
+countryApp.controller('countryDetailController', function($scope, $routeParams){
+	console.log($routeParams);
+});
